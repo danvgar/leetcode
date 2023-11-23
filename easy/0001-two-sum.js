6c1640b8497b2da0ab1a5febfc56e00b932d7e53
Daniel Garcia <danvgar@gmail.com>
2023-11-23 04:05:27 -0800

    Given an integer array and a target integer, return the indices of the two integers whose sum equals the target integer.

--- easy/0001-two-sum.js
+++ easy/0001-two-sum.js
@@ -12,30 +12,24 @@
 
 // You can return the answer in any order.
 
-
-
 // Example 1:
-
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
 
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-// Example 3:
 
+// Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-
 // Constraints:
-
 // 2 <= nums.length <= 104
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
 // Only one valid answer exists.
-
 
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
@@ -89,7 +83,7 @@ var twoSum = function (nums, target) {
             // If map has a number that equals the target number minus the current element in the loop, return that number's index and the current index. 
             return [map.get(target - nums[i]), i];
         } else {
-            // Typically, add key-value pair of element and it's index. 
+            // Typically, add key-value pair: the element and it's index. 
             map.set(nums[i], i);
         }
     }
